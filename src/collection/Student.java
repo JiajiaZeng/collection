87713d77597ca722c133921d0b5767a3f6b91a22
JiajiaZeng <3034177851@qq.com>
2022-01-04 16:18:41 +0800

    collection

---
+++ src/collection/Student.java
@@ -0,0 +1,54 @@
+package collection;
+/**
+ * 创建学生类，实现Comparable接口
+ */
+public class Student implements Comparable<Student> {
+	/**
+	 * 私有属性:学号，姓名，年龄
+	 */
+	private Integer stuId;
+	private String name;
+	private Integer age;
+	/**
+	 *构造方法
+	 */
+	public Student() {
+	}
+	public Student(Integer stuId, String name, Integer age) {
+		this.stuId = stuId;
+		this.name = name;
+		this.age = age;
+	}
+	/**
+	 * getter和setter方法
+	 */
+	public Integer getStuId() {
+		return stuId;
+	}
+	public void setStuId(Integer stuId) {
+		this.stuId = stuId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	@Override
+	public int compareTo(Student stu) {
+		return this.age - stu.age;
+	}
+	public String toString(){
+		return "Student [age = " + age + ",name = " + name +",stuId = " + stuId + "]";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("=====================================");
+	}
+}
